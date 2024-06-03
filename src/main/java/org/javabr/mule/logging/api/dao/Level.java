8e468693f9f8556e00484c5fc223d7b421f647cb
Leo Bitencourt <lbitencourt@gmail.com>
2024-06-03 19:10:26 -0400

    adding api folders

---
+++ src/main/java/org/javabr/mule/logging/api/dao/Level.java
@@ -0,0 +1,49 @@
+package org.javabr.mule.logging.api.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Level {
+
+  DEBUG("DEBUG"),
+  TRACE("TRACE"),
+  INFO("INFO"),
+  WARN("WARN"),
+  ERROR("ERROR");
+
+  private final String value;
+  private final static Map<String, Level> CONSTANTS = new HashMap<String, Level>();
+
+  static {
+    for (Level c : values()) {
+      CONSTANTS.put(c.value, c);
+    }
+  }
+
+  private Level(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return this.value;
+  }
+
+  @JsonValue
+  public String value() {
+    return this.value;
+  }
+
+  @JsonCreator
+  public static Level fromValue(String value) {
+    Level constant = CONSTANTS.get(value);
+    if (constant == null) {
+      throw new IllegalArgumentException(value);
+    } else {
+      return constant;
+    }
+  }
+
+}
---
+++ src/main/java/org/javabr/mule/logging/api/dao/LogConfig.java
@@ -0,0 +1,98 @@
+package org.javabr.mule.logging.api.dao;
+
+import java.util.logging.Logger;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.mule.runtime.extension.api.annotation.param.Optional;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "applicationName",
+    "artifactId",
+    "muleEnvironmentName"
+})
+public class LogConfig {
+
+  private static Logger LOGGER;
+
+  @Parameter
+  @DisplayName("POM ArtifactId")
+  private String artifactId;
+
+  @Parameter
+  @DisplayName("Mule Env Name")
+  @Optional(defaultValue = "${mule.env}")
+  private String muleEnvironmentName;
+
+  @Parameter
+  @DisplayName("Mule Application Name")
+  private String applicationName;
+
+  public void setApplicationName(String applicationName) {
+    this.applicationName = applicationName;
+  }
+
+  @JsonProperty("applicationName")
+  public String getApplicationName() {
+    return applicationName;
+  }
+
+  @JsonProperty("applicationName")
+  public void setMuleEnvironmentName(String muleEnvironmentName) {
+    this.muleEnvironmentName = muleEnvironmentName;
+  }
+
+  @JsonProperty("muleEnvironmentName")
+  public String getMuleEnvironmentName() {
+    return muleEnvironmentName;
+  }
+
+  @JsonProperty("artifactId")
+  public void setArtifactId(String artifactId) {
+    this.artifactId = artifactId;
+  }
+
+  @JsonProperty("artifactId")
+  public String getArtifactId() {
+    return artifactId;
+  }
+
+  public Logger getLogger() {
+    if (LOGGER == null) {
+      LOGGER = Logger.getLogger(artifactId);
+    }
+    return LOGGER;
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder().append(applicationName).append(artifactId).append(muleEnvironmentName).toHashCode();
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (other == this) {
+      return true;
+    }
+    if ((other instanceof LogConfig) == false) {
+      return false;
+    }
+    LogConfig rhs = ((LogConfig) other);
+    return new EqualsBuilder().append(applicationName, rhs.applicationName).append(artifactId, rhs.artifactId)
+        .append(muleEnvironmentName, rhs.muleEnvironmentName).isEquals();
+  }
+
+}
